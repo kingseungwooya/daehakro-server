401c2844292b1f55bb345485fda34417bf0c33bf
kingseungwooya <ksw_0505@nate.com>
2023-07-05 14:13:10 +0900

    initialize: jump into daehakro server

---
+++ src/main/java/project/cnu/daehakro/DaehakroApplication.java
@@ -0,0 +1,13 @@
+package project.cnu.daehakro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaehakroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaehakroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/cnu/daehakro/DaehakroApplicationTests.java
@@ -0,0 +1,13 @@
+package project.cnu.daehakro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaehakroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
