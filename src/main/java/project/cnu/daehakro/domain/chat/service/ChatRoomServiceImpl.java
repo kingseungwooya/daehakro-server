5057528ca51f3f2550f4688df7674acd6a301fdf
kingseungwooya <ksw_0505@nate.com>
2023-07-06 17:31:25 +0900

    feat: 관리자 권한 채팅방 기능 구조 구현

---
+++ src/main/java/project/cnu/daehakro/domain/chat/service/ChatRoomServiceImpl.java
@@ -0,0 +1,27 @@
+package project.cnu.daehakro.domain.chat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import project.cnu.daehakro.domain.chat.repository.ChatRoomRepository;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ChatRoomServiceImpl implements ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    @Override
+    public void createChatRoom(String key) {
+        // sorting 알고리즘.. 어케할건데..
+        chatRoomRepository.deleteAll();
+        // 1단계 관리자 확인 인증
+    }
+
+    @Override
+    public void deleteRooms(String key) {
+        // 1단계 관리자 확인 인증
+        chatRoomRepository.deleteAll();
+    }
+}
