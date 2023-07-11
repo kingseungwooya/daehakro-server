f8af76d574ecfe94220191f04bc72cd3d9c450c3
kingseungwooya <ksw_0505@nate.com>
2023-07-11 17:41:29 +0900

    feat: univ repo 생성

---
+++ src/main/java/project/cnu/daehakro/domain/chat/repository/UnivRepository.java
@@ -0,0 +1,10 @@
+package project.cnu.daehakro.domain.chat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import project.cnu.daehakro.domain.entity.UnivInfo;
+
+@Repository
+public interface UnivRepository extends JpaRepository<Long, UnivInfo> {
+}
+
