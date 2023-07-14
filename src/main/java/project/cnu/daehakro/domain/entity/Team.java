1e19d5e9f6362ba6e8a74d9d4107535ac8ea8f10
kingseungwooya <ksw_0505@nate.com>
2023-07-14 14:14:40 +0900

    feat: member는 팀 단위로 신청한다. team 관련 객체 생성

---
+++ src/main/java/project/cnu/daehakro/domain/entity/Team.java
@@ -0,0 +1,38 @@
+package project.cnu.daehakro.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.cnu.daehakro.domain.enums.MemberSex;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+@Entity
+@Getter
+@NoArgsConstructor
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long teamId;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private MemberSex teamSex;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TeamEvent event;
+
+    private String teamName;
+
+    @Builder
+    public Team(List<Member> members, MemberSex teamSex, TeamEvent event, String teamName) {
+        this.members = members;
+        this.teamSex = teamSex;
+        this.event = event;
+        this.teamName = teamName;
+    }
+}
---
+++ src/main/java/project/cnu/daehakro/domain/entity/TeamEvent.java
@@ -0,0 +1,83 @@
+package project.cnu.daehakro.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+import project.cnu.daehakro.domain.enums.EventType;
+import project.cnu.daehakro.domain.enums.MemberSex;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class TeamEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long eventId;
+
+    private String eventName;
+
+    @CreationTimestamp
+    @Column(name = "create_at", nullable = false)
+    private Timestamp createAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    private int maxApply;
+
+    @Column(name = "open_flag", nullable = false, columnDefinition = "TINYINT", length = 1)
+    private boolean isOpen;
+
+    @Enumerated(EnumType.STRING)
+    private EventType eventType;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "event", cascade = CascadeType.ALL)
+    private List<Team> manTeams = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "event", cascade = CascadeType.ALL)
+    private List<Team> womenTeams = new ArrayList<>();
+
+    @Builder
+    public TeamEvent(String eventName, LocalDate startDate, LocalDate endDate, int maxApply, EventType eventType) {
+        this.eventName = eventName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.maxApply = maxApply;
+        // 생성 즉시 open 할 것인가?
+        this.isOpen = true;
+        this.eventType = eventType;
+    }
+
+    public void close() {
+        this.isOpen = false;
+    }
+
+    public void applyTeam(Team team) {
+
+    }
+
+    public boolean isFull(MemberSex sex) {
+        if(sex.equals(MemberSex.MAN)) {
+            return manTeams.size() == maxApply;
+        }
+        if(sex.equals(MemberSex.WOMAN)) {
+            return womenTeams.size() == maxApply;
+        }
+        return false;
+    }
+}
